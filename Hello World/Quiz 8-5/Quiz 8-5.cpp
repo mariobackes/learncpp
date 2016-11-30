3b9692c17de103e0c6eb9636dee750f3ff19d997
Mario Backes <mario.backes@ise.de>
2016-11-30 15:54:35 +0100

    Add Quiz 8.5

---
+++ Hello World/Quiz 8-5/Quiz 8-5.cpp
@@ -0,0 +1,35 @@
+// Quiz 8-5.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <string>
+
+class Ball
+{
+public:
+    Ball()
+    {
+        m_color = ("Black");
+        m_radius = (10.0);
+    }
+    Ball(s)
+    ~Ball();
+
+private:
+    std::string m_color;
+    double      m_radius;
+};
+
+Ball::Ball()
+{
+}
+
+Ball::~Ball()
+{
+}
+
+int main()
+{
+    return 0;
+}
+
