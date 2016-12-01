b8e744818231e6d902613d3ffa0f2e18ef7467f6
Mario Backes <mario.backes@ise.de>
2016-12-01 10:17:42 +0100

    Finish Quiz 8.5

--- Hello World/Quiz 8-5/Quiz 8-5.cpp
+++ Hello World/Quiz 8-5/Quiz 8-5.cpp
@@ -3,26 +3,42 @@
 
 #include "stdafx.h"
 #include <string>
+#include <iostream>
 
 class Ball
 {
 public:
+    // Constructors:
     Ball()
     {
         m_color = ("Black");
         m_radius = (10.0);
     }
-    Ball(s)
+    Ball(int radius)
+    {
+        m_radius = radius;
+        m_color = "Black";
+    }
+    Ball(std::string color, int radius = 10.0)
+    {
+        m_color = color;
+        m_radius = radius;
+    }
+
+    // Destructor:
     ~Ball();
+
+    /////////////////
+
+    void print()
+    {
+        std::cout << "Color: " << m_color << ", radius: " << m_radius << std::endl;
+    }
 
 private:
     std::string m_color;
     double      m_radius;
 };
-
-Ball::Ball()
-{
-}
 
 Ball::~Ball()
 {
@@ -30,6 +46,18 @@ Ball::~Ball()
 
 int main()
 {
+    Ball def;
+    def.print();
+
+    Ball blue("blue");
+    blue.print();
+
+    Ball twenty(20.0);
+    twenty.print();
+
+    Ball blueTwenty("blue", 20.0);
+    blueTwenty.print();
+
     return 0;
 }
 
